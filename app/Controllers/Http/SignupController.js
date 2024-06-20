df34afca3b42db604d83f44f9f789776d990bf90
KennyWouters <kenny.wouters2@gmail.com>
2024-06-20 16:45:20 +0200

    modifying the verification email sending in signupcontroller with new credentials from .env, not hardcoded

--- app/Controllers/Http/SignupController.js
+++ app/Controllers/Http/SignupController.js
@@ -1,10 +1,13 @@
 // File: app/Controllers/Http/SignupController.js
 'use strict'
 
+const Env = use('Env')
+const smtpUsername = ENV.get('SMTP_USERNAME')
 const User = use('App/Models/User')
 const Token = use('App/Models/Token')
-const nodemailer = require('nodemailer')
+const Mail = use('Mail')
 const crypto = require('crypto')
+
 
 class SignupController {
   async create({ request, response }) {
@@ -23,31 +26,12 @@ class SignupController {
     })
 
     // Send a verification email
-    let transporter = nodemailer.createTransport({
-      host: 'smtp-relay.gmail.com', // replace with your SMTP host
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-        user: 'kenny.wouters2@gmail.com', // replace with your SMTP username
-        pass: ''  // replace with your SMTP password
-      }
-    });
-
-    let mailOptions = {
-      from: '"Sender Name" <sender@example.com>', // sender address
-      to: user.email, // list of receivers
-      subject: 'Please confirm your email address', // Subject line
-      text: 'Please confirm your email address', // plain text body
-      html: `<b>Please confirm your email address by clicking on the following link: <a href="http://13.48.10.251//verify?token=${token}">Verify Email Address</a></b>` // html body
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log('Email sent: ' + info.response);
-      }
-    });
+    await Mail.send('emails.verify', { username: user.username, token: token }, (message) => {
+      message
+        .to(user.email)
+        .from('kenny.wouters2@gmail.com')
+        .subject('Please confirm your email address')
+    })
 
     return response.created(user)
   }
