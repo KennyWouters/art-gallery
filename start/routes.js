bfbf3de99cfaa9eae0009e9a45b70d36d0b1d620
natallya-K <kavalkova@yahoo.fr>
2024-07-15 14:51:43 +0200

    added Order by Account_id

--- start/routes.js
+++ start/routes.js
@@ -9,10 +9,12 @@ Route.group(() => {
   require('./routes/exhibition')
   require('./routes/professional')
   require('./routes/user')
+  require('./routes/order')
   Route.post('/signup', 'SignupController.create')
   Route.get('/signup', ({view}) => view.render('signup'))
   Route.get('/', ({view}) => view.render('home'))
   Route.get('users', 'UserController.index')
   Route.post('users/:id', 'UserController.destroy')
+  Route.get('orders', 'OrderController.index')
 })
 
